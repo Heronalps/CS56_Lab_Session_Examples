3c8c5a7f6f4ab71438eef7447866974a8a96ddf6
michaelzhang <heronalps@gmail.com>
2017-03-03 08:16:43 -0800

    Add Two Demos

--- src/Week_8/AbstractFactoryDemo.java
+++ src/Week_8/AbstractFactoryDemo.java
@@ -42,7 +42,104 @@ public abstract class AbstractFactory {
 }
 
 
+public class ShapeFactory extends AbstractFactory {
+	
+   @Override
+   public Shape getShape(String shapeType){
+   
+      if(shapeType == null){
+         return null;
+      }		
+      
+      if(shapeType.equalsIgnoreCase("RECTANGLE")){
+         return new Rectangle();
+         
+      }else if(shapeType.equalsIgnoreCase("SQUARE")){
+         return new Square();
+      }
+      
+      return null;
+   }
+   
+   @Override
+   Color getColor(String color) {
+      return null;
+   }
+}
 
+public class ColorFactory extends AbstractFactory {
+	
+   @Override
+   public Shape getShape(String shapeType){
+      return null;
+   }
+   
+   @Override
+   Color getColor(String color) {
+   
+      if(color == null){
+         return null;
+      }		
+      
+      if(color.equalsIgnoreCase("RED")){
+         return new Red();
+         
+      }else if(color.equalsIgnoreCase("GREEN")){
+         return new Green();
+      }
+      
+      return null;
+   }
+}
+
+public class FactoryProducer {
+   public static AbstractFactory getFactory(String choice){
+   
+      if(choice.equalsIgnoreCase("SHAPE")){
+         return new ShapeFactory();
+         
+      }else if(choice.equalsIgnoreCase("COLOR")){
+         return new ColorFactory();
+      }
+      
+      return null;
+   }
+}
+
+public class AbstractFactoryPatternDemo {
+   public static void main(String[] args) {
+
+      //get shape factory
+      AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE");
+
+      //get an object of Shape Rectangle
+      Shape shape1 = shapeFactory.getShape("RECTANGLE");
+
+      //call draw method of Shape Rectangle
+      shape1.draw();
+      
+      //get an object of Shape Square 
+      Shape shape2 = shapeFactory.getShape("SQUARE");
+
+      //call draw method of Shape Square
+      shape2.draw();
+
+      //get color factory
+      AbstractFactory colorFactory = FactoryProducer.getFactory("COLOR");
+
+      //get an object of Color Red
+      Color color1 = colorFactory.getColor("RED");
+
+      //call fill method of Red
+      color1.fill();
+
+      //get an object of Color Green
+      Color color2 = colorFactory.getColor("Green");
+
+      //call fill method of Green
+      color2.fill();
+   }
+}
 
 
 
--- src/Week_8/StockDemo.java
+++ src/Week_8/StockDemo.java
@@ -1,5 +1,10 @@
 public class Stock {
 	public int maxProfit(int[] prices) {
-        // write your code here
+        int maxCur = 0, maxSoFar = 0;
+        for(int i = 1; i < prices.length; i++) {
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]); 
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
     }
 }
